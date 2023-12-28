4ff4181ec73f212524fb786a1abfaca7e5833da2
16dipti <150996907+16dipti@users.noreply.github.com>
2023-12-28 20:37:06 +0530

    Add files via upload

---
+++ Task4-chatbot/long_response.py
@@ -0,0 +1,7 @@
+import random
+
+R_EATING = "I don't like eating anything because I'm a bot obviously"
+
+def unknown():
+    response = ['Could you please rephrase that? ', '...', 'Sounds about right', 'What does that mean?'][random.randrange(4)]
+    return response
